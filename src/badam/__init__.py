bb31b118232925982aa527127d29a201d472c86b
Jonery <qijunluo@link.cuhk.edu.cn>
2024-06-19 11:20:56 +0800

    rename callback.

--- src/badam/__init__.py
+++ src/badam/__init__.py
@@ -1,1 +1,2 @@
-from .block_optim import BlockOptimizer, BlockOptimizerRatio, clip_grad_norm_for_sparse_tensor
+from .block_optim import BlockOptimizer, BlockOptimizerRatio
+from .utils import clip_grad_norm_old_version, BAdamCallback
--- src/badam/utils.py
+++ src/badam/utils.py
@@ -1,25 +1,104 @@
 from transformers.trainer_callback import TrainerCallback
 from transformers.utils import logging
-
+from transformers.integrations import is_deepspeed_zero3_enabled
+from typing import Union, Iterable
+import torch
 
 logger = logging.get_logger(__name__)
 
-class BAdamZeRO3Callback(TrainerCallback):
+# class BAdamZeRO3Callback(TrainerCallback):
+class BAdamCallback(TrainerCallback):
     """ Handler for setup BAdam's training process with ZeRO-3. """
     def __init__(self, *args, **kwargs):
         self.init_loss_scale = kwargs.get("init_loss_scale", 12)
         
     def on_train_begin(self, *args, **kwargs):
+        if is_deepspeed_zero3_enabled():
+            optimizer = kwargs["optimizer"] # DeepSpeedOptimizerWrapper
+            
+            # Create the BlockOptimizer's reference to DeepSpeedZeroOptimizer_Stage3
+            ds_optim = optimizer.optimizer # DeepSpeedZeroOptimizer_Stage3
+            badam_optim = ds_optim.optimizer # BlockOptimizer
+            badam_optim.ds_optimizer = ds_optim
         
-        optimizer = kwargs["optimizer"] # DeepSpeedOptimizerWrapper
-        
-        # Create the BlockOptimizer's reference to DeepSpeedZeroOptimizer_Stage3
-        ds_optim = optimizer.optimizer # DeepSpeedZeroOptimizer_Stage3
-        badam_optim = ds_optim.optimizer # BlockOptimizer
-        badam_optim.ds_optimizer = ds_optim
-        
-        # adjust the loss scale when it is not specified in the configuration file
-        if not hasattr(ds_optim, "dynamic_loss_args"):
-            ds_optim.cur_scale = 2**self.init_loss_scale
-            logger.info(f"Reducing initial loss scale to {ds_optim.cur_scale} for avoiding unnecessary attempts.")
+            # adjust the loss scale when it is not specified in the configuration file
+            if not hasattr(ds_optim, "dynamic_loss_args"):
+                ds_optim.cur_scale = 2**self.init_loss_scale
+                logger.info(f"Reducing initial loss scale to {ds_optim.cur_scale} for avoiding unnecessary attempts.")   
+
+
+# For torch>=2.1, `_foreach_norm` is used when implementing `clip_grad_norm_`, which doesn't support sparse tensor yet.
+# We can temporarily fix this issue by using the older torch version's implementation:
+    # self.accelerator.clip_grad_norm_ = MethodType(clip_grad_norm_for_sparse_tensor, self.accelerator)
+def clip_grad_norm_old_version(self, parameters, max_norm, norm_type=2):
+    """
+    Modification of the accelerator.clip_grad_norm_ to enable gradient clipping for sparse tensor.
+    Used for torch version >= 2.1
+    """
+    from accelerate.utils import DistributedType
+    from torch import inf
+
+    if self.distributed_type == DistributedType.FSDP:
+        self.unscale_gradients()
+        parameters = [p for p in parameters]
+        for model in self._models:
+            if parameters == [p for p in model.parameters()]:
+                return model.clip_grad_norm_(max_norm, norm_type)
+    elif self.distributed_type == DistributedType.DEEPSPEED:
+        # `accelerator.backward(loss)` is doing that automatically. Therefore, its implementation is not needed
+        # We cannot return the gradient norm because DeepSpeed does it.
+        return None
+    self.unscale_gradients()
     
+    def clip_func_(
+        parameters: Union[torch.Tensor, Iterable[torch.Tensor]], max_norm: float, norm_type: float = 2.0,
+        error_if_nonfinite: bool = False) -> torch.Tensor:
+        r""" torch 1.13 version clip_grad_norm_, works well with sparse tensor.
+        Clips gradient norm of an iterable of parameters.
+
+        The norm is computed over all gradients together, as if they were
+        concatenated into a single vector. Gradients are modified in-place.
+
+        Args:
+            parameters (Iterable[Tensor] or Tensor): an iterable of Tensors or a
+                single Tensor that will have gradients normalized
+            max_norm (float or int): max norm of the gradients
+            norm_type (float or int): type of the used p-norm. Can be ``'inf'`` for
+                infinity norm.
+            error_if_nonfinite (bool): if True, an error is thrown if the total
+                norm of the gradients from :attr:`parameters` is ``nan``,
+                ``inf``, or ``-inf``. Default: False (will switch to True in the future)
+
+        Returns:
+            Total norm of the parameter gradients (viewed as a single vector).
+        """
+        
+        if isinstance(parameters, torch.Tensor):
+            parameters = [parameters]
+        grads = [p.grad for p in parameters if p.grad is not None]
+        max_norm = float(max_norm)
+        norm_type = float(norm_type)
+        if len(grads) == 0:
+            return torch.tensor(0.)
+        device = grads[0].device
+        if norm_type == inf:
+            norms = [g.detach().abs().max().to(device) for g in grads]
+            total_norm = norms[0] if len(norms) == 1 else torch.max(torch.stack(norms))
+        else:
+            total_norm = torch.norm(torch.stack([torch.norm(g.detach(), norm_type).to(device) for g in grads]), norm_type)
+        if error_if_nonfinite and torch.logical_or(total_norm.isnan(), total_norm.isinf()):
+            raise RuntimeError(
+                f'The total norm of order {norm_type} for gradients from '
+                '`parameters` is non-finite, so it cannot be clipped. To disable '
+                'this error and scale the gradients by the non-finite norm anyway, '
+                'set `error_if_nonfinite=False`')
+        clip_coef = max_norm / (total_norm + 1e-6)
+        # Note: multiplying by the clamped coef is redundant when the coef is clamped to 1, but doing so
+        # avoids a `if clip_coef < 1:` conditional which can require a CPU <=> device synchronization
+        # when the gradients do not reside in CPU memory.
+        clip_coef_clamped = torch.clamp(clip_coef, max=1.0)
+        for g in grads:
+            g.detach().mul_(clip_coef_clamped.to(g.device))
+        return total_norm
+    
+    return clip_func_(parameters, max_norm, norm_type=norm_type)
